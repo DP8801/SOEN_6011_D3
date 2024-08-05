e6b5cd8fe21370a0463fd7792913c4e73cb6e99e
Dev Pandya <71974172+DP8801@users.noreply.github.com>
2024-08-05 10:25:14 -0400

    moved docs from the folder which were under git ignore

---
+++ src/main/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"ExponentialCalculator"},{"p":"org.example","l":"ExponentialCalculatorTest"}];updateSearchResults();
